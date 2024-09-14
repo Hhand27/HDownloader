0bcc010ee35cc12ea505c9de7704813ec936c7ba
Hand27 <Leonardo1712@proton.me>
2024-09-13 20:50:21 -0600

    Add files via upload
    Esto facilita la instalacion de yt-dlp, el cual se usa para descargar los videos.

---
+++ ytdlp_downloader.py
@@ -0,0 +1,42 @@
+import subprocess
+
+def main():
+    while True:
+        cmd = input("¿Qué distribución usa (ingrese el número)? \n"
+                    " 1 Debian/Ubuntu \n"
+                    " 2 Fedora \n"
+                    " 3 Arch/Manjaro/EndeavourOS \n"
+                    " 4 OpenSUSE \n"
+                    " 5 Gentoo \n"
+                    " 6 RHEL Based (CentOS, AlmaLinux, RockyLinux) \n"
+                    " 7 Solus \n"
+                    " c Salir \n")
+
+        if cmd == "1":
+            print("Debian/Ubuntu")
+            subprocess.run("sudo apt install yt-dlp", shell=True)
+        elif cmd == "2":
+            print("Fedora")
+            subprocess.run("sudo dnf install yt-dlp", shell=True)
+        elif cmd == "3":
+            print("Arch, Manjaro, EndeavourOS")
+            subprocess.run("sudo pacman -S yt-dlp", shell=True)
+        elif cmd == "4":
+            print("OpenSUSE")
+            subprocess.run("sudo zypper install yt-dlp", shell=True)
+        elif cmd == "5":
+            print("Gentoo")
+            subprocess.run("sudo emerge yt-dlp", shell=True)
+        elif cmd == "6":
+            print("RHEL Based")
+            subprocess.run("sudo dnf install yt-dlp", shell=True)
+        elif cmd == "7":
+            print("Solus")
+            subprocess.run("sudo eopkg install yt-dlp", shell=True)
+        elif cmd == "c":
+            print("Saliendo...")
+            break
+        else:
+            print("Ingrese una respuesta válida, como por ejemplo '2'.")
+
+main()
