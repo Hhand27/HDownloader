36e0ab4f3fca99b1e5e5b7bd8894fa2a3b9ebf99
Hand27 <Leonardo1712@proton.me>
2024-09-17 23:41:41 -0600

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import subprocess
 
-while True:
-    cmd = input("mp4 o mp3: ").lower()
+def videomp3():
+    subprocess.run(f"yt-dlp -x --audio-format mp3 {link}", shell=True)
 
+def videomp4():
+    subprocess.run(f"yt-dlp -f best --recode-video mp4 {link}", shell=True)
+    print("¡Done!")
+
+while True:
+    cmd = input("mp4, mp3 o exit : ").lower()
+
+    if cmd == "exit":
+        exit()
+        
     link = input("Ingrese un link: ")
 
     if cmd == "mp3":
-        subprocess.run(f"yt-dlp -x --audio-format mp3 {link}", shell=True)
+        videomp3()
     elif cmd == "mp4":
-        subprocess.run(f"yt-dlp {link}", shell=True)
-    elif cmd == "exit":
+        videomp4()
+    elif link == "exit":
         exit()
     else:
         print("Comando no válido, elige 'mp3' o 'mp4'.")
